2feb251832975b2a5aaac0f6f29595ba15b225b6
SohamDesai1 <sohamdesai123@gmail.com>
2022-07-05 19:56:28 +0530

    Fetch API

---
+++ fetch.js
@@ -0,0 +1,9 @@
+console.log("FETCH API");
+function getData() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+        .then(response => response.json())
+        .then(data => console.log(data))
+        // .catch(error => console.log(error));
+}
+getData();
+
