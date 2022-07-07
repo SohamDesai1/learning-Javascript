807fe38c73d7b9b119b1f279bd751643b1d02736
SohamDesai1 <sohamdesai123@gmail.com>
2022-07-08 00:52:22 +0530

    Map

---
+++ Map.js
@@ -0,0 +1,8 @@
+const first = 'C Programming', second = 'Java', third = 'Python';
+
+const myMap = new Map();
+myMap.set(first, 'C Programming');
+myMap.set(second, 'Java');
+myMap.set(third, 'Python');
+
+console.log(myMap);
